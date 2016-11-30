e1ac29ae1bcdf3448361ab9ceec4832bf0962f97
Andrea Francia <andrea@andreafrancia.it>
2016-11-30 12:27:40 +0100

    Tweaked legacy code

--- src/BookShop.php
+++ src/BookShop.php
@@ -12,19 +12,21 @@ class BookShop {
             new Book("Harry Potter And The Goblet Of Fire", 18.28),
             new Book("Harry Potter and the Order of the Phoenix", 18.29)
         );
-        $this->orderTotal = 0;
+        $this->orderTotal = 0.0;
         $this->totalBooks = 0;
         $this->receipt = '';
     }
 
     public function buy($title) {
         $price = 0;
+        $this->receipt .= "$title";
         for($i=0; $i<sizeof($this->books); $i++) {
-            if($title = $this->books[$i]) {
+            if($title = $this->books[$i]->getTitle()) {
                 $price = $this->books[$i]->getFullPrice();
             }
         }
-        $this->receipt += "$title $price EUR\n";
+        $price = sprintf("%2.2d", $price);
+        $this->receipt .= ": $price EUR\n";
         $this->orderTotal+=$price;
         $this->totalBooks++;
         if($this->totalBooks > 3) {
@@ -32,8 +34,13 @@ class BookShop {
         }
     }
 
-    public function total() {
+    private function getTotal() {
         return $this->orderTotal;
+    }
+
+    public function getReceipt() {
+        $total = $this->getTotal();
+        return "$this->receipt\nTOTAL: $total EUR";
     }
 }
 
