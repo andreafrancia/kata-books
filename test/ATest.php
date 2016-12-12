658b2d0c9c84d55be407a1786c1d13c2285f8d83
Andrea Francia <andrea@andreafrancia.it>
2016-12-12 16:26:23 +0100

    Aggiunto altro test

--- test/ATest.php
+++ test/ATest.php
@@ -10,4 +10,20 @@ class ATest extends TestCase {
 
 TOTAL: 18 EUR", $shop->getReceipt());
     }
+
+    public function  test_more_than_three_books() {
+        $shop = new BookShop();
+
+        $shop->buy("Harry Potter and the Half-Blood Prince");
+        $shop->buy("Harry Potter and the Half-Blood Prince");
+        $shop->buy("Harry Potter and the Half-Blood Prince");
+        $shop->buy("Harry Potter and the Half-Blood Prince");
+
+        self::assertEquals('Harry Potter and the Half-Blood Prince: 18 EUR
+Harry Potter and the Half-Blood Prince: 18 EUR
+Harry Potter and the Half-Blood Prince: 18 EUR
+Harry Potter and the Half-Blood Prince: 18 EUR
+
+TOTAL: 57.6 EUR', $shop->getReceipt());
+    }
 }
