425ef843209de3c174d48bb0e7c6a4d2123e1881
Andrea Francia <andrea@andreafrancia.it>
2016-12-12 16:28:32 +0100

    Refactor

--- test/ATest.php
+++ test/ATest.php
@@ -5,9 +5,11 @@ use PHPUnit\Framework\TestCase;
 class ATest extends TestCase {
     private $shop;
 
-    public function test_something() {
+    public function setUp(){
         $this->shop = new BookShop();
+    }
 
+    public function test_something() {
         $this->shop->buy("Harry Potter and the Half-Blood Prince");
 
         self::assertEquals("Harry Potter and the Half-Blood Prince: 18 EUR
@@ -16,7 +18,6 @@ TOTAL: 18 EUR", $this->shop->getReceipt());
     }
 
     public function  test_more_than_three_books() {
-        $this->shop = new BookShop();
 
         $this->shop->buy("Harry Potter and the Half-Blood Prince");
         $this->shop->buy("Harry Potter and the Half-Blood Prince");
